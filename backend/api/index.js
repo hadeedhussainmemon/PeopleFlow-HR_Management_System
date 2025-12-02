091c02a0d9cae7661767a7ed5c924ceb87c46821
hadeedhussainmemon <programmerhadeed@gmail.com>
2025-12-02 15:50:22 +0500

    checking if fixed

--- backend/api/index.js
+++ backend/api/index.js
@@ -21,8 +21,6 @@ const connectToDatabase = async () => {
       throw new Error('MONGO_URI is not defined.');
     }
     const db = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
       serverSelectionTimeoutMS: 10000, // 10-second timeout
     });
     cachedDb = db;
